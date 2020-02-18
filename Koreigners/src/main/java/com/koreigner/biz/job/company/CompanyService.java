2decae5d25359982bd58e4354e4424dc15687854
rexypark <hotboa3091@naver.com>
2020-02-18 10:07:18 +0900

    REXy
    asdf

--- Koreigners/src/main/java/com/koreigner/biz/job/company/CompanyService.java
+++ Koreigners/src/main/java/com/koreigner/biz/job/company/CompanyService.java
@@ -1,5 +1,12 @@
 package com.koreigner.biz.job.company;
 
-public interface CompanyService {
+import java.util.List;
 
+public interface CompanyService {
+	List<CompanyVO> getCateList();
+	List<CompanyVO> getCateChild(String cate_kor);
+	List<CompanyVO> getAddrDoList();
+	List<CompanyVO> getAddrSiList(String si_kor);
+	
+	
 }
