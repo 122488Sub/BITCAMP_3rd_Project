40afdad65b5e38bb07137ed5e70e34f38ea288fd
myungsub <yello0105@gmail.com>
2020-04-07 14:01:09 +0900

    Auto stash before merge of "develop" and "origin/develop"

--- Koreigners/src/main/webapp/resources/js/house/houseMainRespon.js
+++ Koreigners/src/main/webapp/resources/js/house/houseMainRespon.js
@@ -13,31 +13,39 @@ function checkSize() {
 	// 800 이하인지 if문으로 확인
 	if (width_size <= 1300) {
 		changeMinSizeUtilities();
-	} else if(width_size<1960){
+	} else if(width_size<1960 && width_size>1300){
 		changeMaxSizeUtilities();
 	}
 }
 function changeMinSizeUtilities() {
 	
 	$("#map").css('float', 'none');
-	$("#map").css('weight','100%');
+	$("#map").css('width','350px');
 	$("#map").css('margin','0 auto');
+	$("#map").css('height','450px');
+	
 	$("#responDiv").css('height', '900px');
+	$("#content").css('height', '900px');
+	
 	$("#option_panel").css('float', 'none');
-	$("#option_panel").css('weight','100%');
+	$("#option_panel").css('width','100%');
 	$("#option_panel").css('margin','0 auto');
+	
 	$(".table_dosi").css('margin','0 auto');
 	$("#filterBox").css('margin','0 auto');
 	
 }
 function changeMaxSizeUtilities() {
-	$("#map").css('weight','35%');
+	$("#map").css('width','35%');
 	$("#map").css('float', 'left');
-	$("#map").css('margin','0 '+((Number(width_size)-1300)/4)+'px');
-	$("#responDiv").css('height', '450px');
-	$("#option_panel").css('margin','0 auto');
-	$("#option_panel").css('weight','60%');
+	$("#map").css('height','450px');
+	$("#map").css('margin','0 auto');
 	
+	$("#content").css('height', '450px');
+	$("#responDiv").css('height', '450px');
+	
+	$("#option_panel").css('margin','0 auto');
+	$("#option_panel").css('width','60%');
 	$("#option_panel").css('float', 'left');
 	
 	
